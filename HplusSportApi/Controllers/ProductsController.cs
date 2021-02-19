b886e8b48ae5d6c1117f645763fdd3c516eca86d
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-02-18 19:29:44 -0800

    Add files via upload

--- HplusSportApi/Controllers/ProductsController.cs
+++ HplusSportApi/Controllers/ProductsController.cs
@@ -3,52 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HplusSportApi.Models;
+using HplusSport.Api.Classes;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace HplusSportApi.Controllers
 {
-    [Route("/products/{id}")]
-    [ApiController]
+    //THIS IS THE CONSTRACTOR of the shop contact 
+    //using this ensurecreated to ensure that you have create an point of the data an can be retributed to the code
+    //this is for see the data
+    //return all  the products and will show on an array
 
+
+    [Route("[controller]")]
+    [ApiController]
     public class ProductsController : ControllerBase
     {
         private readonly ShopContext _context;
-        //THIS IS THE CONSTRACTOR of the shop contact 
+
         public ProductsController(ShopContext context)
         {
-
-            //using this ensurecreated to ensure that you have create an point of the data an can be retributed to the code
             _context = context;
-            //this is for see the data
+
             _context.Database.EnsureCreated();
-
         }
-        [HttpGet("{id}")]
 
-        public IActionResult GetAllProducts(int id)
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts([FromQuery] ProductQueryParameters queryParameters)
         {
-            //return all  the products and will show on an array
-            var product = _context.Products.Find(id);
+            IQueryable<product> products = _context.Products;
+
+            if (queryParameters.MinPrice != null &&
+                queryParameters.MaxPrice != null)
+            {
+                products = products.Where(
+                    p => p.Price >= queryParameters.MinPrice.Value &&
+                         p.Price <= queryParameters.MaxPrice.Value);
+            }
+            //Impramentating Name on the ITEMS 
+            if (!string.IsNullOrEmpty(queryParameters.Sku))
+            {
+                products = products.Where(p => p.Sku == queryParameters.Sku);
+            }
+
+            if (!string.IsNullOrEmpty(queryParameters.Name))
+            {
+                products = products.Where(
+                    p => p.Name.ToLower().Contains(queryParameters.Name.ToLower()));
+            }
+
+            products = products
+                .Skip(queryParameters.Size * (queryParameters.Page - 1))
+                .Take(queryParameters.Size);
+
+            return Ok(await products.ToArrayAsync());
+        }
+
+        [HttpGet("{id}")]
+        //get the erros that can appear if we dont have the ID
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
             if (product == null)
             {
                 return NotFound();
             }
-            return OK(product);
-
-
-
+            return Ok(product);
         }
-
-        private IActionResult OK(object product)
-        {
-            throw new NotImplementedException();
-        }
-
-
     }
+
+
+
+
+
+
 
 
 }
