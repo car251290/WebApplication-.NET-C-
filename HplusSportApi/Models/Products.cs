3918dc5e31827a09a960e4f5ec2e877709ef759a
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-02-20 15:29:51 -0800

    Add files via upload

--- HplusSportApi/Models/Products.cs
+++ HplusSportApi/Models/Products.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -9,12 +10,14 @@ namespace HplusSportApi.Models
 {
     public class product
     {
-       public int Id { get; set; }
+        public int Id { get; set; }
 
         public string Sku { get; set; }
-
-        public string Name { get; set;}
-
+        //for make the product is necessary
+        [Required]
+        public string Name { get; set; }
+        //for make a restriction with the strings
+        [MaxLength(255)]
         public string Description { get; set;}
 
         public decimal Price { get; set; }
