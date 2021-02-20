d2de2c9154d6f4dbebd9b030c76884442b1c8844
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-02-20 15:30:49 -0800

    Add files via upload

--- HplusSportApi/Controllers/ProductsController.cs
+++ HplusSportApi/Controllers/ProductsController.cs
@@ -20,6 +20,7 @@ namespace HplusSportApi.Controllers
 
     [Route("[controller]")]
     [ApiController]
+
     public class ProductsController : ControllerBase
     {
         private readonly ShopContext _context;
@@ -34,16 +35,16 @@ namespace HplusSportApi.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAllProducts([FromQuery] ProductQueryParameters queryParameters)
         {
-            IQueryable<product> products = _context.Products;
+            IQueryable<product> products = (IQueryable<product>)_context.Products;
+            
 
             if (queryParameters.MinPrice != null &&
                 queryParameters.MaxPrice != null)
             {
-                products = products.Where(
-                    p => p.Price >= queryParameters.MinPrice.Value &&
+                products = products.Where( p => p.Price >= queryParameters.MinPrice.Value &&
                          p.Price <= queryParameters.MaxPrice.Value);
             }
-            //Impramentating Name on the ITEMS 
+            //Impramentating Name on the ITEMS this is for the search tearm
             if (!string.IsNullOrEmpty(queryParameters.Sku))
             {
                 products = products.Where(p => p.Sku == queryParameters.Sku);
@@ -54,7 +55,7 @@ namespace HplusSportApi.Controllers
                 products = products.Where(
                     p => p.Name.ToLower().Contains(queryParameters.Name.ToLower()));
             }
-
+            // products and search skip
             products = products
                 .Skip(queryParameters.Size * (queryParameters.Page - 1))
                 .Take(queryParameters.Size);
@@ -63,7 +64,7 @@ namespace HplusSportApi.Controllers
         }
 
         [HttpGet("{id}")]
-        //get the erros that can appear if we dont have the ID
+        //get that can appear if we dont have the ID
         public async Task<IActionResult> GetProduct(int id)
         {
             var product = await _context.Products.FindAsync(id);
@@ -73,13 +74,63 @@ namespace HplusSportApi.Controllers
             }
             return Ok(product);
         }
+        //to post the Products 
+        [HttpPost]
+        public async Task<ActionResult<product>> PostProduct([FromBody] product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                "GetProduct", new { id = product.Id },
+            product
+            );
+        }
+
+        [HttpPut("{id}")]
+
+        public async Task<IActionResult> PutProduct([FromRoute]int id, [FromBody] product product)
+        {
+            if (id != product.Id) {
+                return BadRequest();
+            }
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (_context.Products.Find(id) == null)
+                {
+                    return NotFound();
+                }
+                throw;
+            }
+            return NotContent();
+        }
+
+        private IActionResult NotContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<product>> DeleteProduct(int id) {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+
+        }
+
     }
-
-
-
-
-
-
-
 
 }
